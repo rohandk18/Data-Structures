abf9c5850635400e74e29c20d38cdf5d6d60b55e
Rohan kumbhojkar <63917050+rohandk18@users.noreply.github.com>
2020-10-11 17:17:11 +0530

    Add files via upload

---
+++ LinkedList2.cpp
@@ -0,0 +1,62 @@
+//============================================================================
+// Name        : LinkedListCalcNodes.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+struct node
+{
+	int data;
+	node*next;
+};
+class LinkedList
+{
+	node*first = NULL;
+	node*last;
+	node*Node;
+public:
+	void create()
+	{
+		first = new node();
+		cout<<"Enter the data to be inserted into the node";
+		cin>>first->data;
+		first->next = NULL;
+		last = first;
+		int addnode;
+		cout<<"Do you want to continue?";
+	    cin>>addnode;
+		while(addnode)
+		{
+			Node = new node();
+			cout<<"Enter the data for the node";
+			cin>>Node->data;
+			cout<<"Do you want to continue?";
+		    cin>>addnode;
+			Node->next = NULL;
+			last->next = Node;
+			last = Node;
+		}
+		Display(first);
+	}
+	void Display(node*p)
+	{
+		while(p!=NULL)
+		{
+			cout<<p->data;
+			p = p->next;
+		}
+	}
+};
+int main()
+{
+	LinkedList *list = new LinkedList();
+    list->create();
+
+
+
+	return 0;
+}
