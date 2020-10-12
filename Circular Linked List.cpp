07cca945e0db3035d1ccd60275611ab2d1de2cfe
Rohan kumbhojkar <63917050+rohandk18@users.noreply.github.com>
2020-10-12 21:06:56 +0530

    Add files via upload

---
+++ Circular Linked List.cpp
@@ -0,0 +1,60 @@
+//============================================================================
+// Name        : Circular.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+struct node
+{
+	int data;
+	node*next;
+};
+class LinkedList
+{
+	node*first,*last,*temp;
+public:
+	void create()
+	{
+		first = new node();
+		cout<<"Enter the data";
+		cin>>first->data;
+		//first->next = NULL; //----Use this if it was a singly linked List--------
+		first->next = first;
+		last = first;
+		cout<<"Do you want to continue?";
+		int addnode;
+		cin>>addnode;
+		while(addnode)
+		{
+			temp = new node();
+			cout<<"Enter the data";
+			cin>>temp->data;
+			temp->next = last->next;
+			last->next = temp;
+			last = temp;
+			cout<<"Do you want to continue?";
+			cin>>addnode;
+		}
+		Display(first);
+	}
+
+	void Display(node*p)
+	{
+		do
+		{
+			cout<<p->data;
+		    p = p->next;
+		}
+		while(p!=first);
+	}
+  };
+int main()
+{
+	LinkedList *list = new LinkedList();
+	list->create();
+	return 0;
+}
